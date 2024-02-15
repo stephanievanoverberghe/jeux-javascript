8d465ead0f1ea051d81ca1464f05bc7b84dc0886
Stéphanie Vanoverberghe <orangestreet@live.fr>
2024-02-15 16:14:22 +0100

    exercice 3

--- public/assets/js/reservation-system.js
+++ public/assets/js/reservation-system.js
@@ -7,20 +7,6 @@ d'attente si un livre est déjà réservé.
 
 Consignes :
 
-1 - Créer un objet bibliotheque contenant des paires clé/valeur où les clés sont les noms des livres et les 
-valeurs sont des objets contenant les propriétés disponible (booléen) et listeAttente (tableau 
-    d'utilisateurs en attente).
-
-2 - Écrire une fonction reserverLivre(nomDuLivre, utilisateur) qui vérifie si le livre est disponible. Si 
-oui, le marque comme indisponible et affiche un message de confirmation. Sinon, ajoute l'utilisateur à la 
-liste d'attente.
-
-3 - Écrire une fonction annulerReservation(nomDuLivre, utilisateur) qui annule la réservation d'un livre 
-et, si une liste d'attente existe, attribue le livre au premier utilisateur en liste d'attente.
-
-4 - Utiliser des conditions complexes et la gestion des objets pour contrôler la logique de réservation et 
-d'annulation.
-
 5 - Tester le système avec plusieurs scénarios de réservation et d'annulation.
  */
 
@@ -50,5 +36,33 @@ function reserveBooks(nameBook, user) {
     } else {
         book.waitingList.push(user);
         console.log(`Le livre "${nameBook}" est actuellement indisponible. ${user} a été ajouter à la liste d'attente.`);
+    }
+}
+
+/**
+ * Fonction qui annule la réservation d'un livre.
+ * Si une liste d'attente existe, attribue le livre au premier utilisateur en liste d'attente.
+ * 
+ * @param {string} nameBook 
+ * @param {string} user 
+ */
+function cancelReservation(nameBook, user) {
+    let book = library[nameBook];
+    if (!book.disponibility && book.waitingList.length > 0) {
+        if (book.waitingList.includes(user)) {
+            book.waitingList = book.waitingList.filter(u => u !== user);
+            console.log(`Le réservation du livre "${nameBook}" a été annulé par ${user}.`);
+            if (book.waitingList.length > 0) {
+                let nextUser = book.waitingList.shift();
+                console.log(`Le livre "${nameBook}" est maintenant reservé pour ${nextUser}.`);
+            } else {
+                book.disponibility === true;
+            }
+        } else {
+            console.log(`Aucune réservation trouvée pour ${user} sur le livre "${nameBook}".`);
+        }
+    } else {
+        book.disponibility === true;
+        console.log(`La réservation du livre "${nameBook}" a été annulé et le livre est maintenant disponible.`);
     }
 }
